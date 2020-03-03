0c18e6a579e9de19a27663258f92b51bb5df9190
MOBOLA MORONTO <t2wycemo@gmail.com>
2020-03-03 18:59:49 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,6 +1,16 @@
 class Dog 
   attr_accessor :name, :breed, :age
+  DOGS = []
   
-  def self.new_from_db_row
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed 
+    @age = age
+    DOGS << self 
+  end
+  
+  def self.all
+    DOGS
+  end
   
 end  
